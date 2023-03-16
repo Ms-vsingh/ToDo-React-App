e130ad448382594db8226883adbf98be9f460c0f
Ms-vsingh <singh.varsha91195@gmail.com>
2023-03-16 17:16:06 +0530

    Added HashRouter

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import "./index.css";
 import App from "./App";
 import Users from "./Users";
 // import List from "./List";
-import { BrowserRouter, Routes, Route } from "react-router-dom"; //to connect your app to the browser's URL
+import { HashRouter, Routes, Route } from "react-router-dom"; //to connect your app to the browser's URL
 
 const root = ReactDOM.createRoot(document.getElementById("main"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/users" element={<Users />} />
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
